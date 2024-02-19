f0bc038ee59230ddfdfb7d71f9e72249a0f40533
Ruslan <diego.ruslan.rus@gmail.com>
2024-02-19 10:39:27 +0300

    [+] Изменения в коде по заданию для ветки Feature-branch

--- lecture-two/src/main/java/ru/mts/homework/Main.java
+++ lecture-two/src/main/java/ru/mts/homework/Main.java
@@ -1,7 +1,11 @@
 package ru.mts.homework;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        out.println("Hello world");
+        out.println("Я люблю МТС");
+
     }
 }
