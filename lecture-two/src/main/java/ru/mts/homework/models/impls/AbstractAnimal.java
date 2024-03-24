10a69c712993b0a037ffc9c3c4e10dd71a642433
Ruslan <diego.ruslan.rus@gmail.com>
2024-03-24 15:47:15 +0300

    HW-1: исправление ошибок

--- lecture-two/src/main/java/ru/mts/homework/models/impls/AbstractAnimal.java
+++ lecture-two/src/main/java/ru/mts/homework/models/impls/AbstractAnimal.java
@@ -2,7 +2,7 @@ package ru.mts.homework.models.impls;
 
 import ru.mts.homework.repositories.Animal;
 
-public class AbstractAnimal implements Animal {
+public abstract class AbstractAnimal implements Animal {
 
     protected String breed; // порода
     protected String name; // имя
